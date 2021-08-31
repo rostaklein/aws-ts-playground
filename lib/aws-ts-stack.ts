f80f6f7c843867bd53491f7905806e792d7f1f32
Rostislav Klein <rostislav.klein@pipedrive.com>
2021-08-31 21:51:47 +0200

    aws cdk test init

--- lib/aws-ts-stack.ts
+++ lib/aws-ts-stack.ts
@@ -1,9 +1,28 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import { Bucket } from "@aws-cdk/aws-s3";
+import { CfnOutput, CfnParameter, Duration } from "@aws-cdk/core";
 
 export class AwsTsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const duration = new CfnParameter(this, "duration", {
+      type: "Number",
+      default: 6,
+      minValue: 1,
+      maxValue: 10,
+    });
+
+    const myBucket = new Bucket(this, "some-bucket", {
+      lifecycleRules: [
+        {
+          expiration: Duration.days(duration.valueAsNumber),
+        },
+      ],
+    });
+
+    new CfnOutput(this, "myBucket", {
+      value: myBucket.bucketName,
+    });
   }
 }
