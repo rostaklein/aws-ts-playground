d0e5dfb8452e29a13f74316d4b18dd80147bb632
Rostislav Klein <rostislav.klein@pipedrive.com>
2021-09-12 15:07:49 +0200

    real app stack init

--- bin/aws-ts.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import "source-map-support/register";
-import * as cdk from "@aws-cdk/core";
-import { AwsTsStack } from "../lib/aws-ts-stack";
-
-const app = new cdk.App();
-new AwsTsStack(app, "AwsTsStack", {});
