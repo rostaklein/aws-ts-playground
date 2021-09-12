25730c2ac4006ddc4ab6dab87c37a9478b143106
Rostislav Klein <rostislav.klein@pipedrive.com>
2021-09-12 15:45:28 +0200

    lambda init

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,8 +1,28 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
+import {
+  Code,
+  Function as LambdaFn,
+  Runtime,
+} from "aws-cdk-lib/lib/aws-lambda";
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/lib/aws-apigateway";
+import { join } from "path";
 
 export class SpaceStack extends Stack {
+  private api = new RestApi(this, "SpaceApi");
+
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
+
+    const helloLambda = new LambdaFn(this, "HelloLambda", {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
+      handler: "hello.main",
+    });
+
+    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaResource = this.api.root.addResource("hello");
+
+    helloLambdaResource.addMethod("GET", helloLambdaIntegration);
   }
 }
